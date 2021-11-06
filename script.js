4531f8b326ab36e59efe8f85eafba622414420c4
Sweta Karar <52580094+karar189@users.noreply.github.com>
2021-11-07 04:33:52 +0530

    added the counter logic

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //this is javascript file
+//this is javascript file
+const monthEl = document.getElementById("months");
+const daysEl = document.getElementById("days");
+
+const festDay = "1 jan 2022";
+
+function countdown() {
+    const festDayFinal = new Date(festDay);
+    const currentDate = new Date();
+    const totalSeconds = (festDayFinal - currentDate) / 1000;
+    const month = Math.floor(totalSeconds / 3600 / 24) /30 ;
+    const months = (Math.round(month));
+    const day = Math.floor(totalSeconds / 3600 / 24);
+    const days = Math.round(day - 30);
+
+    monthEl.innerHTML = months;
+    daysEl.innerHTML = days;
+
+}
+
+// initial call of counter
+countdown();
+
+setInterval(countdown, 1000);
+
+
+  
